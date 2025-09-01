facf35db6cdd73eed2f140ff35b2404c3fafea3c
arunkumar66 <arunkumarg9816@gmail.com>
2025-09-01 11:10:48 +0530

    strings.js

---
+++ strings.js
@@ -0,0 +1,96 @@
+str = "sowmya"
+
+str = 'bhevin'
+
+str = `arun`
+
+//coersion = number convert to string
+
+
+num = 2+ 2
+
+console.log(num)
+
+num = "2"+ 2
+
+console.log(num)
+
+num3 = "omkar"
+console.log(num3)
+
+num3 = 4-2
+console.log(num3)
+
+num3 = "4"-2
+console.log(num3)
+
+num3 = "arun"-2
+console.log(num3)
+
+//NAN = not a number 
+
+num4 = 7+undefined
+console.log(num4)
+
+
+//.length //its a method
+str = "sowmya"
+
+console.log(str.length)//4
+
+str = "bhevin is a good boy"
+console.log(str.length)
+
+
+str = "    hfhj    "
+console.log(str.trim())      //space remove starting trin start method trim end end space delete
+
+//charAt()
+
+// str ="40"
+// num = number(str)
+
+// console.log(typeof(num))
+// num2 =number("89")
+
+// includes method
+str = "h,j,k,l,"
+
+console.log(str.includes("k"))
+
+str = "h,j,k,l,"
+
+console.log(str.includes("a"))
+
+//strings are immutable = cznnot be replacable is not possible
+
+// replace method
+str1 = "$196"
+str2 = "$345"
+
+total = str1+str2
+
+console.log(total)
+
+str1 = "$196"
+str2 = "$345"
+
+total = str1+str2
+
+console.log(total)
+
+str1 = "$196"
+str2 = "$345"
+
+total = str1+str2
+
+console.log(total)
+
+str1 = "$196"
+str2 = "$345"
+
+total = str1.replace("$","")+str2.replace("$","")
+
+console.log(total)
+
+
