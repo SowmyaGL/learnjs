c496e8b0350b0e738b16ae84450662b1e46385c9
arunkumar66 <arunkumarg9816@gmail.com>
2025-09-02 18:25:51 +0530

    numbers.js

---
+++ numbers.js
@@ -0,0 +1,55 @@
+num = 45
+
+str = Number("45")
+
+num = String(86)
+//convert the number to string
+
+console.log(typeof(num))
+
+//bigInt = any a number above 2^53
+
+num = 2**53
+
+console.log(num)
+
+num2 = 143247395894866957676878n
+
+num3 = 1n
+
+console.log(num2+num3)
+//number is object
+
+num = 335
+
+//NaN
+
+//isNaN()//true or false
+
+//parseInt
+
+str = "34.5"
+num3 = parseInt(str)
+
+console.log(num3)
+
+//parseFloat 34.6
+
+
+
+max = Math.max(1,2,3,4,5,6)
+
+console.log(max)
+
+minNum = Math.min(3,4,5,6,7)
+
+console.log(minNum)
+
+num = Math.round(3.6)
+console.log(num)
+
+num2 = Math.round(3.5)
+console.log(num2)
+
+num3 = Math.ceal(3,2)
+console.log(num3)
