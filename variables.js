2f5bccf06b760c27982141692faf051ceb71e929
ArunkumarG66 <arunkumarg9816@gmail.com>
2026-01-19 19:49:15 +0530

    functons.js changes

--- variables.js
+++ variables.js
@@ -9,3 +9,20 @@ console.log(z)
 // ===================================================
 
 console.log("sowmya")
+
+stuname = "sowmya"
+
+//3 key words available 
+//var
+//let
+//const
+
+var greet = "HI"
+if(5>3){
+    console.log(greet)
+}
+
+//reuseable it is advantage
+
+//reasign the different value is possible
+
